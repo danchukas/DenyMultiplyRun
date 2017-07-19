be854900cda8eb8e6717ef513ea870d8c906bf5d
danchukas <volk160590@mail.ru>
2017-07-19 16:35:38 +0300

    add sensiolab

--- tests/Mock/TypeList.php
+++ tests/Mock/TypeList.php
@@ -20,28 +20,48 @@ class TypeList
      */
     public static function getMockList(array $need_type_list = [])
     {
-        $type_list = [];
-
         if (empty($need_type_list)) {
-            $need_type_list = [
-                TypeEnum::ARRAY
-                , TypeEnum::BOOLEAN
-                , TypeEnum::DOUBLE
-                , TypeEnum::INTEGER
-                , TypeEnum::NULL
-                , TypeEnum::OBJECT
-                , TypeEnum::RESOURCE
-                , TypeEnum::STRING
-                , TypeEnum::UNKNOWN
-            ];
+            $need_type_list = self::getDefaultList();
         }
+
+        $type_list = self::getTypeMockList($need_type_list);
+
+        return $type_list;
+    }
+
+    /**
+     * @return array
+     */
+    private static function getDefaultList()
+    {
+        $need_type_list = [
+            TypeEnum::ARRAY
+            , TypeEnum::BOOLEAN
+            , TypeEnum::DOUBLE
+            , TypeEnum::INTEGER
+            , TypeEnum::NULL
+            , TypeEnum::OBJECT
+            , TypeEnum::RESOURCE
+            , TypeEnum::STRING
+            , TypeEnum::UNKNOWN
+        ];
+
+        return $need_type_list;
+    }
+
+    /**
+     * @param array $need_type_list
+     * @return array
+     */
+    private static function getTypeMockList(array $need_type_list): array
+    {
+        $type_list = [];
 
         foreach ($need_type_list as $type_mock) {
             $class = 'DanchukAS\Mock\Type\\' . ucfirst($type_mock) . 'TypeMock';
             /** @var \DanchukAS\Mock\TypeMock $class */
             $type_list[$type_mock] = $class::getGenerator();
         }
-
         return $type_list;
     }
 }
